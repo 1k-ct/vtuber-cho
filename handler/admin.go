cc1bed0d859b05fbbf6acfa5701d67e7adfcb959
1k-ct <268iop@gmail.com>
2021-06-20 00:32:28 +0900

    add admin handlers

--- handler/admin.go
+++ handler/admin.go
@@ -22,63 +22,18 @@ type ResponseJson struct {
 }
 
 func FitchJwt(c *gin.Context) {
-	// LoginInfo := &LoginInfo{}
-	// if err := c.BindJSON(&LoginInfo); err != nil {
-	// 	c.JSON(400, appErrors.ErrMeatdataMsg(err, appErrors.ErrorJSON))
-	// 	return
-	// }
-
-	// api key の取得
-	// apikey := c.Param("api_key")
-
-	// if apikey == "" {
-	// 	c.JSON(400, &ResponseJson{
-	// 		Status:  "400",
-	// 		Message: "The api key is required",
-	// 		Data:    nil,
-	// 	})
-	// }
-	// // 取得したapi key から、hash　を生成する
-	// hash, err := bcrypt.GenerateFromPassword([]byte(apikey), 10)
-	// if err != nil {
-	// 	c.JSON(http.StatusBadRequest,
-	// 		&ResponseJson{
-	// 			Status:  "400",
-	// 			Message: "There is a problem with the api_key.",
-	// 			Data:    nil,
-	// 		})
-	// }
-	// apikey = string(hash)
-
-	// .envにあるapi key と　hash を比較
-
-	// loginInfo := &LoginInfo{}
-	// if err := c.ShouldBindJSON(&loginInfo); err != nil {
-	// 	c.JSON(http.StatusUnprocessableEntity, appErrors.ErrMeatdataMsg(err, appErrors.ErrorJSON))
-	// 	return
-	// }
-
 	apiKey := c.Request.Header.Get("api-key")
-	// user, err := ah.accountUseCase.FindByID(u.ID)
-	// if err != nil {
-	// 	if errors.Is(err, gorm.ErrRecordNotFound) {
-	// 		c.JSON(http.StatusUnauthorized, appErrors.ErrMeatdataMsg(err, appErrors.ErrRecordDatabase))
-	// 		return
-	// 	}
-	// 	c.JSON(http.StatusInternalServerError, appErrors.ServerError)
-	// 	return
-	// }
 	if err := godotenv.Load(); err != nil {
 		c.JSON(500, appErrors.ErrMeatdataMsg(err, appErrors.ServerError))
 		return
 	}
-	// log.Println(apiKey)
-	log.Println("api-key is", os.Getenv("API_KEY"))
+
 	if os.Getenv("API_KEY") != apiKey {
-		c.JSON(400, gin.H{"status": "Invalid key"})
+		c.JSON(401, gin.H{"status": "Invalid key"})
 		return
 	}
-	ts, err := jwtToken.CreateToken("admin", "admin", os.Getenv("SECRET_KEY"), os.Getenv("REFRESH_KEY"))
+
+	ts, err := jwtToken.CreateToken(os.Getenv("USER_ID"), os.Getenv("USER_NAME"), os.Getenv("SECRET_KEY"), os.Getenv("REFRESH_KEY"))
 	if err != nil {
 		c.JSON(http.StatusUnprocessableEntity, appErrors.ErrNotCreateToken)
 		return
@@ -87,8 +42,7 @@ func FitchJwt(c *gin.Context) {
 		"access_token":  ts.AccessToken,
 		"refresh_token": ts.RefreshToken,
 	}
-	c.JSON(http.StatusOK, tokens)
-
+	c.JSON(200, tokens)
 }
 func RegisterVtuber(c *gin.Context) {
 	// ---------------------------
@@ -122,7 +76,7 @@ func RegisterVtuber(c *gin.Context) {
 	vtubers := []*Vtuber{}
 	res := db.Where("channel_id = ?", requestVtuber.ChannelID).Find(&vtubers)
 	if res.RowsAffected != 0 {
-		c.JSON(400, gin.H{"status": "すでにあります。"})
+		c.JSON(400, gin.H{"status": "It's already created"})
 		return
 	}
 	if res.Error != nil {
@@ -143,13 +97,5 @@ func RegisterVtuber(c *gin.Context) {
 			return
 		}
 	}
-	c.JSON(200, vtuber)
-	// TODO:
-	// if user.UserName == "" {
-	// 	c.JSON(400, gin.H{"msg": "UserNameは必須です。"})
-	// 	return
-	// }
-	// if user.Password == "" {
-	// 	c.JSON(http.StatusBadRequest, gin.H{"msg": "Passwordは必須です。"})
-	// }
+	c.JSON(201, vtuber)
 }
---
+++ handler/admin_test.go
@@ -0,0 +1,8 @@
+package handler
+
+import (
+	"testing"
+)
+
+func TestGenerateFromPassword(t *testing.T) {
+}
