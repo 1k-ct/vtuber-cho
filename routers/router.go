d83cf77d43465a7eba35bf65e8806915989d8031
1k-ct <268iop@gmail.com>
2021-06-20 00:28:32 +0900

    add router

--- routers/router.go
+++ routers/router.go
@@ -1,8 +1,6 @@
 package routers
 
 import (
-	"net/http"
-
 	"github.com/1k-ct/twitter-dem/pkg/middelware"
 	"github.com/1k-ct/vtuber-cho/handler"
 	"github.com/gin-gonic/gin"
@@ -12,31 +10,20 @@ func NewRouter(secretKey string) *gin.Engine {
 	r := gin.Default()
 
 	// admin
-	// TODO api-key と　password をハッシュ化して保存
-	// TODO ハッシュ化したものと比べる
-	// TODO database を変更
+
 	// jwtを所得
-	// GET api/v1/admin
 	r.GET("/api/v1/admin", handler.FitchJwt)
 
 	// jwt check group --middleware--
 	// |- vtuber type 登録
-	// |- post api/v1/vtubers
 	r.POST("/api/v1/vtubers", middelware.TokenAuthMiddleware(secretKey), handler.RegisterVtuber)
 
 	// users
 
-	// api/v1/vtubers/affiliations/types
 	r.GET("/api/v1/vtubers/:affiliations/:types", handler.FitchRandVtuber)
 
 	// 検索 query
-	// post api/v1/search&q=
+	r.GET("api/v1/search", handler.SearchVtuber)
 
 	return r
 }
-
-func ping(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"status": "ok"})
-}
-
-//
