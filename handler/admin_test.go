11f9600dab4119d678ed47e5e43af58ed27a9d95
1k-ct <268iop@gmail.com>
2021-06-20 20:40:03 +0900

    add test

--- handler/admin_test.go
+++ handler/admin_test.go
@@ -1,8 +1,82 @@
-package handler
+package handler_test
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 	"testing"
+
+	"github.com/1k-ct/vtuber-cho/handler"
+	"github.com/joho/godotenv"
 )
 
 func TestGenerateFromPassword(t *testing.T) {
+
+}
+func TestJsonReadFile(t *testing.T) {
+	vDataBytes, err := ioutil.ReadFile("../vtuber-data/vtuber-req.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(vDataBytes)
+}
+
+type requestVtubers struct {
+	Data []handler.RequestVtuber `json:"data"`
+}
+
+func TestReqestVtuberJsonFile(t *testing.T) {
+	reqVtubers := &requestVtubers{}
+	vDataBytes, err := ioutil.ReadFile("../vtuber-data/vtuber-req.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := json.Unmarshal(vDataBytes, reqVtubers); err != nil {
+		t.Fatal(err)
+	}
+	url := "http://localhost:8080/api/v1/vtubers"
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+	accessToken := os.Getenv("ACCESS_TOKEN")
+	for _, v := range reqVtubers.Data {
+		vData, _ := json.Marshal(v)
+		body, err := testPostVtuber(url, accessToken, vData)
+		if err != nil {
+			t.Fatal(err)
+		}
+		log.Println(body)
+	}
+}
+func testPostVtuber(url, accessToken string, jsonStr []byte) (string, error) {
+	req, err := http.NewRequest(
+		"POST",
+		url,
+		bytes.NewBuffer(jsonStr),
+	)
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("authorization", accessToken)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	return string(body), err
 }
