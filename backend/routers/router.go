9e30075c76c52d8e8560410ab1b2cd70d9f3e9fe
1k-ct <268iop@gmail.com>
2021-06-25 01:15:18 +0900

    fix abbreviation url /api

--- backend/routers/router.go
+++ backend/routers/router.go
@@ -4,30 +4,29 @@ import (
 	"github.com/1k-ct/twitter-dem/pkg/middelware"
 	"github.com/1k-ct/vtuber-cho/backend/handler"
 
-	// "github.com/1k-ct/vtuber-cho/handler"
 	"github.com/gin-gonic/gin"
 )
 
 func NewRouter(secretKey string) *gin.Engine {
 	r := gin.Default()
-
+	v1 := r.Group("v1")
 	// admin
 
 	// jwtを所得
 	// /admin?API_KEY=******
-	r.GET("/api/v1/admin", handler.FitchJwt)
+	v1.GET("/admin", handler.FitchJwt)
 
 	// jwt check group --middleware--
 	// |- vtuber type 登録
-	r.POST("/api/v1/vtubers", middelware.TokenAuthMiddleware(secretKey), handler.RegisterVtuber)
+	v1.POST("/vtubers", middelware.TokenAuthMiddleware(secretKey), handler.RegisterVtuber)
 
 	// r.POST("/api/v1/vtubers-file", handler.RegisterVtuberJsonFile)
 	// users
 
-	r.GET("/api/v1/vtubers/:affiliations/:types", handler.FitchRandVtuber)
+	v1.GET("/vtubers/:affiliations/:types", handler.FitchRandVtuber)
 
 	// 検索 query
-	r.GET("api/v1/search", handler.SearchVtuber)
+	v1.GET("/search", handler.SearchVtuber)
 
 	return r
 }
