bc85453d25c97f0787707b27cc8f412dd55b3463
1k-ct <268iop@gmail.com>
2021-07-29 16:18:59 +0900

    fix database register type

--- backend/handler/vtuber.go
+++ backend/handler/vtuber.go
@@ -6,21 +6,73 @@ import (
 	"time"
 
 	"github.com/1k-ct/twitter-dem/pkg/appErrors"
+	"github.com/1k-ct/twitter-dem/pkg/database"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
+	"golang.org/x/xerrors"
 )
 
+type VtuberType struct {
+	gorm.Model
+	VtuberID uint
+	Types    string
+}
 type Vtuber struct {
 	gorm.Model
 	Name        string
 	ChannelID   string
 	Affiliation string
+	VtuberTags  []VtuberTag `gorm:"ForeingKey:VtuberTagID"`
 }
-type VtuberType struct {
+type VtuberTag struct {
 	gorm.Model
 	VtuberID uint
-	Types    string
+	Tag      string
+}
+
+func FetchDatabaseVtuber(affiliation, tag string) ([]Vtuber, error) {
+	// database connection localhost
+	// -----------------
+	config, err := database.NewLocalDB("user", "password", "sample")
+	if err != nil {
+		return nil, xerrors.New(err.Error())
+	}
+	db, err := config.Connect()
+	if err != nil {
+		return nil, xerrors.New(err.Error())
+	}
+	defer db.Close()
+	// -------------------
+	vtubers := []Vtuber{}
+	if err := db.Model(&Vtuber{}).Where("affiliation = ?", affiliation).Preload("VtuberTags", "tag = ?", tag).Find(&vtubers).Error; err != nil {
+		return nil, xerrors.New(err.Error())
+	}
+	for i, v := range vtubers {
+		if len(v.VtuberTags) == 0 {
+			vtubers = append(vtubers[:i], vtubers[i+1:]...)
+		}
+	}
+	return vtubers, nil
+}
+func RegisterDatabaseVtuber(v *Vtuber) (*Vtuber, error) {
+	// database connection localhost
+	// -----------------
+	config, err := database.NewLocalDB("user", "password", "sample")
+	if err != nil {
+		return nil, xerrors.New(err.Error())
+	}
+	db, err := config.Connect()
+	if err != nil {
+		return nil, xerrors.New(err.Error())
+	}
+	defer db.Close()
+	// -------------------
+	if err := db.Create(&v).Error; err != nil {
+		return nil, xerrors.New(err.Error())
+	}
+
+	return v, nil
 }
 
 // FitchRandVtuber /:affiliations/:types 条件に合ったvtuberをランダムに紹介する
@@ -35,48 +87,23 @@ func FitchRandVtuber(c *gin.Context) {
 	}
 	defer db.Close()
 
-	// db　から条件に合ったvtuberを取得
-	vtuber := &Vtuber{}
-	vtubers := []*Vtuber{}
-	if err := db.Model(&vtuber).Where("affiliation = ?", vAffiliation).Find(&vtubers).Error; err != nil {
-		c.JSON(400, gin.H{"status": "not found affiliation"})
-		return
-	}
-	vtuberIDs := []uint{}
-	for _, v := range vtubers {
-		vtuberIDs = append(vtuberIDs, v.ID)
-	}
-
-	vtuberType := &VtuberType{}
-	vtuberTypes := []*VtuberType{}
-	if err := db.Model(&vtuberType).Where("types = ?", vType).Find(&vtuberTypes).Error; err != nil {
+	// // db　から条件に合ったvtuberを取得
+	vtubers, err := FetchDatabaseVtuber(vAffiliation, vType)
+	if err != nil {
 		c.JSON(400, gin.H{"status": "not found affiliation"})
 		return
 	}
 
-	res := []uint{}
-	for i, vid := range vtuberIDs {
-		for _, v := range vtuberTypes {
-			if vid == v.VtuberID {
-				res = append(res, uint(i))
-			}
-		}
-	}
-
-	if len(res) == 0 {
-		c.JSON(400, gin.H{"status": "not found vtuber"})
-		return
-	}
 	if len(vtubers) == 0 {
 		c.JSON(400, gin.H{"status": "not found vtuber"})
 		return
 	}
 
 	rand.Seed(time.Now().UnixNano())
-	n := rand.Intn(len(res))
+	n := rand.Intn(len(vtubers))
 
-	url := "https://www.youtube.com/channel/" + vtubers[res[n]].ChannelID
-	c.JSON(200, gin.H{"name": vtubers[res[n]].Name, "url": url})
+	url := "https://www.youtube.com/channel/" + vtubers[n].ChannelID
+	c.JSON(200, gin.H{"name": vtubers[n].Name, "url": url})
 }
 func SearchVtuber(c *gin.Context) {
 	db, err := DatabaseConnection()
--- backend/handler/vtuber_test.go
+++ backend/handler/vtuber_test.go
@@ -1,8 +1,61 @@
 package handler_test
 
 import (
+	"fmt"
 	"testing"
+
+	"github.com/1k-ct/twitter-dem/pkg/database"
+	"github.com/1k-ct/vtuber-cho/backend/handler"
+	_ "github.com/go-sql-driver/mysql"
+	"golang.org/x/xerrors"
 )
 
 func TestFitchRandVtuber(t *testing.T) {
+}
+func testAutomigrad() error {
+	config, err := database.NewLocalDB("user", "password", "sample")
+	if err != nil {
+		return xerrors.New(err.Error())
+	}
+	db, err := config.Connect()
+	if err != nil {
+		return xerrors.New(err.Error())
+	}
+	defer db.Close()
+
+	if err := db.DropTableIfExists(&handler.Vtuber{}, &handler.VtuberTag{}).Error; err != nil {
+		return xerrors.New(err.Error())
+	}
+	if err := db.AutoMigrate(&handler.Vtuber{}).Error; err != nil {
+		return xerrors.New(err.Error())
+	}
+	if err := db.AutoMigrate(&handler.VtuberTag{}).AddForeignKey("vtuber_id", "vtubers(id)", "CASCADE", "CASCADE").Error; err != nil {
+		return xerrors.New(err.Error())
+	}
+	return nil
+}
+func TestMain(t *testing.T) {
+	vtubers, err := handler.FetchDatabaseVtuber("hololive", "tag1")
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(vtubers)
+}
+
+func TestRegisterData(t *testing.T) {
+	v := handler.Vtuber{
+		Name:        "name",
+		ChannelID:   "channel id",
+		Affiliation: "honey strap",
+		VtuberTags: []handler.VtuberTag{
+			{Tag: "tag1"},
+			// {Tag: "tag2"},
+			// {Tag: "tag3"},
+		},
+	}
+	data, err := handler.RegisterDatabaseVtuber(&v)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(data)
 }
